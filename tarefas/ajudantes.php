4578443b3d70bddab5de561fb4e0a8f33a2bf083
Lucas Malves <lmalves10@gmail.com>
2021-12-06 18:59:21 -0300

    Capítulos 7 e 8 parte 1
    Capítulos 7 e 8 realizados, porem ainda sem as atividades concluídas
    Banco de dados já criados

---
+++ tarefas/ajudantes.php
@@ -0,0 +1,51 @@
+<?php
+
+function traduz_data_para_banco($data)
+{
+    if ($data == "") {
+        return "0000-00-00";
+    }
+
+    $objeto_data = DateTime::createFromFormat('d/m/Y', $data);
+
+    return $objeto_data->format('Y-m-d');
+}
+
+function traduz_data_para_exibir($data)
+{
+    if ($data == "" or $data == "0000-00-00") {
+        return "";
+    }
+
+    $objeto_data = DateTime::createFromFormat('Y-m-d', $data);
+    
+    return $objeto_data->format('d/m/Y');
+}
+
+function traduz_prioridade($codigo)
+{
+    $prioridade = '';
+
+    switch($codigo){
+        case 1:
+            $prioridade = 'Baixa';
+            break;
+        case 2:
+            $prioridade = 'Média';
+            break;
+        case 3:
+            $prioridade = 'Alta';
+            break;
+    }
+    
+    return $prioridade;
+}
+
+function traduz_concluida($concluida)
+{
+    if ($concluida == 1) {
+        return 'Sim';
+    }
+
+    return 'Não';
+}
--- tarefas/template.php
+++ tarefas/template.php
@@ -24,14 +24,14 @@
                 <fieldset>
                     <legend>Prioridade:</legend>
                     <label>
-                        <input type="radio" name="prioridade" value="baixa" checked /> Baixa
-                        <input type="radio" name="prioridade" value="media" /> Média
-                        <input type="radio" name="prioridade" value="alta" /> Alta
+                        <input type="radio" name="prioridade" value="1" checked /> Baixa
+                        <input type="radio" name="prioridade" value="2" /> Média
+                        <input type="radio" name="prioridade" value="3" /> Alta
                     </label>
                 </fieldset>
                 <label>
                     Tarefa concluída:
-                    <input type="checkbox" name="concluida" value="sim" />
+                    <input type="checkbox" name="concluida" value="1" />
                 </label>
                 <input type="submit" value="Cadastrar" />
             </fieldset>
@@ -48,9 +48,9 @@
                 <tr>
                     <td><?php echo $tarefa['nome']; ?></td>
                     <td><?php echo $tarefa['descricao']; ?></td>
-                    <td><?php echo $tarefa['prazo']; ?></td>
-                    <td><?php echo $tarefa['prioridade']; ?></td>
-                    <td><?php echo $tarefa['concluida']; ?></td>
+                    <td><?php echo traduz_data_para_exibir($tarefa['prazo']); ?></td>
+                    <td><?php echo traduz_prioridade($tarefa['prioridade']); ?></td>
+                    <td><?php echo traduz_concluida($tarefa['concluida']); ?></td>
                 </tr>
             <?php endforeach; ?>
         </table>
